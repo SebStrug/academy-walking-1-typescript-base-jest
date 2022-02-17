509f79add7a8eaef91cf36f04a15b967786e545c
Ron Werner <ron.werner@cazoo.com>
2022-02-17 16:37:57 +0100

    Squashed commit of the following:
    commit 30860a8fcf26ec80fe890efad55c1d2805c1b922
    Author: Ron Werner <ron.werner@cazoo.com>
    Date:   Thu Feb 17 16:37:12 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 2e60bee8813b5b9fc318f2fc168247ed56897f11
    Author: Sebastian Strug <strugsebastian@gmail.com>
    Date:   Thu Feb 17 15:30:06 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 93ec61707676459a61488c9d8325225b78161f6f
    Author: Sebastian Strug <strugsebastian@gmail.com>
    Date:   Thu Feb 17 15:29:48 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit ad637e5b4752b215078b038dc69e19446b038596
    Author: Ron Werner <ron.werner@cazoo.com>
    Date:   Thu Feb 17 16:27:14 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 7e0f141421b6d2ce20356e04e0587f7d9c337633
    Author: Sebastian Strug <strugsebastian@gmail.com>
    Date:   Thu Feb 17 15:18:12 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit d5d53d8d99ea54c1627c649de2bce7b90cfa00a4
    Author: Ron Werner <ron.werner@cazoo.com>
    Date:   Thu Feb 17 16:14:27 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 83945918ca17d331a62c5819d975775b44895b99
    Author: Sebastian Strug <strugsebastian@gmail.com>
    Date:   Thu Feb 17 15:02:00 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit ca20c4195d839b6217422fb0e3275b291cf8cf3b
    Author: Ron Werner <ron.werner@cazoo.com>
    Date:   Thu Feb 17 15:56:46 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit fc3ad9613c602d381f24510a3b28a365465bb37b
    Author: Sebastian Strug <strugsebastian@gmail.com>
    Date:   Thu Feb 17 14:47:00 2022 +0000
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    Co-authored-by: Sebastian Strug <strugsebastian@gmail.com>

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,19 @@
 export class Example {
-    add(a: number, b: number) {
-        return a + b;
+    returnString(a: number) {
+        if ((a < 1) || (a > 100)) {
+            return "";
+        }
+        else if (a % 3 == 0 && a % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if (a % 3 == 0) {
+            return "Fizz";
+        } 
+        else if (a % 5 == 0) {
+            return "Buzz"
+        }
+        else {
+            return a.toString();
+        }
     }
 }
