ccd9097e99fba052f71f879d77c92d7e615320c7
Alex Burley <alexburley3@live.co.uk>
2022-03-17 16:23:10 +0000

    Failing test board of three

--- src/main/week6/gameoflife.ts
+++ src/main/week6/gameoflife.ts
@@ -1,7 +1,15 @@
+export type Counter = "*" | " ";
 export class GameOfLife {
-  board: ["*" | " "][];
-  constructor(x: number, y: number, seed: [number, number][]) {
-    this.board = new Array(y).fill(new Array(x).fill(""));
+  board: Counter[][];
+  constructor(x: number, y: number, seed: [number, number][] = []) {
+    // this.board = new Array(y).fill(new Array(x).fill(" "));
+    this.board = [
+      [" ", " "],
+      [" ", " "],
+    ];
+    for (let s of seed) {
+      this.board[s[0]][s[1]] = "*";
+    }
   }
   tick(): GameOfLife {
     return this;
--- src/test/week6/gameoflife.test.ts
+++ src/test/week6/gameoflife.test.ts
@@ -8,4 +8,27 @@ describe("example test", () => {
       [" ", " "],
     ]);
   });
+
+  it("Should return an empty board with seed", () => {
+    let sut = new GameOfLife(2, 2, [
+      [0, 0],
+      [0, 1],
+    ]);
+    expect(sut.board).toEqual([
+      ["*", "*"],
+      [" ", " "],
+    ]);
+  });
+
+  it("Should return a board of three with seed", () => {
+    let sut = new GameOfLife(3, 3, [
+      [0, 0],
+      [0, 1],
+    ]);
+    expect(sut.board).toEqual([
+      ["*", "*", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ]);
+  });
 });
