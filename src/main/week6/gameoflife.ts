91c2cc0f4d955814efadb98b287a26dc661ab1f9
Alex Burley <alexburley3@live.co.uk>
2022-03-17 16:28:29 +0000

    Initialise board

--- src/main/week6/gameoflife.ts
+++ src/main/week6/gameoflife.ts
@@ -2,11 +2,13 @@ export type Counter = "*" | " ";
 export class GameOfLife {
   board: Counter[][];
   constructor(x: number, y: number, seed: [number, number][] = []) {
-    // this.board = new Array(y).fill(new Array(x).fill(" "));
-    this.board = [
-      [" ", " "],
-      [" ", " "],
-    ];
+    this.board = [];
+    for (let i = 0; i < y; i++) {
+      this.board.push([]);
+      for (let j = 0; j < x; j++) {
+        this.board[i].push(" ");
+      }
+    }
     for (let s of seed) {
       this.board[s[0]][s[1]] = "*";
     }
